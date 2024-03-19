8aba4f3d40f321f7b7d6094c08cc5d9b9a9fe0c1
Everton Deplá <evertondutra257@gmail.com>
2024-03-19 16:38:50 -0300

    adição UseEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import axios from 'axios';
 import People from './assets/people.svg';
@@ -23,25 +23,29 @@ function App() {
   const inputAge = useRef();
 
   async function addNewUser() {
-   const data = await axios.post("http://localhost:3001/users", {
-     name:inputName.current.value,
-      age:inputAge.current.value,
-   });
-   
-   console.log(data)
-    /* setUsers([
-      ...users,
-      {
-        id: Math.random(),
-        name: inputName.current.value,
-        age: inputAge.current.value,
-      },
-    ]);*/
+    const { data: newUser } = await axios.post("http://localhost:3001/users", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+    });
+
+    setUsers([ ...users, newUser]);
   }
 
-  function deleteUser(userId){
-    const newUsers = users.filter( user => user.id !== userId );
+useEffect(() => {
+  async function fetchUsers(){
+    const {data: newUsers} = await axios.get('http://localhost:3001/users');
     
+    setUsers(newUsers);
+  }
+  
+  fetchUsers();
+
+}, []);
+
+
+  function deleteUser(userId) {
+    const newUsers = users.filter(user => user.id !== userId);
+
     setUsers(newUsers);
   }
 
